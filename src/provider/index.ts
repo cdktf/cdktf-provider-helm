b609a7e1532521245b2d5bda22cccca84d5c9aee
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-10-13 12:57:46 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@f0b85b7

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/helm/2.11.0/docs
 // generated from terraform resource schema
 
