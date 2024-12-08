cfb554d8adf0d260647c714e20d0d11f2cb420b3
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-12-08 06:05:15 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-helm-template/index.ts
+++ src/data-helm-template/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/helm/2.16.1/docs/data-sources/template
 // generated from terraform resource schema
 
